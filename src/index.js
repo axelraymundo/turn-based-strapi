fe017692ffa89e1632a6fb863d59bd30c233c450
Axel Raymundo <stephen.raymundo@gmail.com>
2023-04-27 15:33:05 +0800

    replay and env

--- src/index.js
+++ src/index.js
@@ -18,7 +18,19 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {
+  async bootstrap(/*{ strapi }*/) {
+    console.log("FIRST FUNCTION CALLED");
+
+    const entry = await strapi.db
+      .query("plugin::users-permissions.user")
+      .findOne({
+        select: ["username"],
+        where: { id: 1 },
+        populate: { replays: { populate: { actions: true } } },
+      });
+
+    console.log("Player", entry.replays[0].actions);
+
     io.on("connection", async (socket) => {
       console.log(`User[${socket.id}] connected`);
 
